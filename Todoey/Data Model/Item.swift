3c7c7c1f7191e89194c58e0f4a310437ae68f087
Raymond Linn <rlinn@alpine-la.com>
2019-02-26 09:54:00 -0800

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Raymond Linn on 2/21/19.
+//  Copyright © 2019 Raymond Linn. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+  var title: String = ""
+  var done: Bool = false
+}
